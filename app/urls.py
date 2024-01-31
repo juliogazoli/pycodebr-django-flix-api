87770699bf196fd9967c2ac1b7fc16ddd89a1b1a
Julio Gazoli <julio_gazoli@hotmail.com>
2024-01-31 17:38:08 -0300

    hello world

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+
+def hello_view(request):
+    return JsonResponse({'message': 'Hello World!'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_view),
 ]
