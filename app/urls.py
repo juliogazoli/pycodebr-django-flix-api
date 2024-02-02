7afe5bfe666fa3462e867dee93e367497abcd572
Julio Gazoli <julio_gazoli@hotmail.com>
2024-02-02 16:21:20 -0300

    genre view post method

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,5 @@ from genres.views import genre_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre-list'),
+    path('genres/', genre_view, name='genre'),
 ]
