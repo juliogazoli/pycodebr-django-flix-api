a583145efbfa143a8a2faf6481f8d34615ee5cab
Julio Gazoli <julio_gazoli@hotmail.com>
2024-04-03 16:07:53 -0300

    rate with aggregate and avg

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 from genres.models import Genre
@@ -24,16 +25,10 @@ class MovieModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_rate(self, obj):
-        reviews = obj.reviews.all()
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
         
-        if reviews:
-            sum_reviews = 0
-            for review in reviews:
-                sum_reviews += review.stars
-            
-            reviews_count = reviews.count()
-
-            return round(sum_reviews / reviews_count, 1)
+        if rate:
+            return round(rate, 1)
         
         return None
 
