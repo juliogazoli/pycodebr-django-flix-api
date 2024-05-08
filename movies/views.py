ed652a13e9a8f14fb7442e580bb8862ea2c71ff5
Julio Gazoli <julio_gazoli@hotmail.com>
2024-05-08 17:07:56 -0300

    data of movie stats

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Count, Avg
 from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
+from reviews.models import Review
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
@@ -22,10 +24,19 @@ class MovieStatsView(views.APIView):
     queryset = Movie.objects.all()
 
     def get(self, request):
-        # Buscar todos os dados
-        # Montar resposta
-        # Devolve resposta para o user com estatísticas
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values(
+            'genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(
+            avg_stars=Avg('stars'))['avg_stars']
+
         return response.Response(
-            data={'message': 'Funcionou'},
+            data={
+                'total_movies': total_movies,
+                'movies_by_genre': movies_by_genre,
+                'total_reviews': total_reviews,
+                'average_stars': round(average_stars, 1) if average_stars else 0,
+            },
             status=status.HTTP_200_OK,
         )
