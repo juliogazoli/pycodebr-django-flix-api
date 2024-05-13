009db6612f222e9af0bd8760a4ae6701598f923b
Julio Gazoli <julio_gazoli@hotmail.com>
2024-05-13 19:18:17 -0300

    import actors by django commands

--- actors/management/commands/import_actors.py
+++ actors/management/commands/import_actors.py
@@ -1,4 +1,7 @@
+import csv
+from datetime import datetime
 from django.core.management.base import BaseCommand, CommandParser
+from actors.models import Actor
 
 
 class Command(BaseCommand):
@@ -12,4 +15,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_name = options['file_name']
-        print(f'MEU PRIMEIRO COMMAND: file_name: {file_name}')
+
+        with open(file_name, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                birthday = datetime.strptime(row['birthday'], '%Y-%m-%d').date()
+                nationality = row['nationality']
+
+                self.stdout.write(self.style.NOTICE(name))
+
+                Actor.objects.create(
+                    name=name,
+                    birthday=birthday,
+                    nationality=nationality,
+                )
+
+        self.stdout.write(self.style.SUCCESS('ATORES IMPORTADOS COM SUCESSO!'))
